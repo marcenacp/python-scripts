b80c399c02b245d95765d332c154b49f9756dc07
Pierre Marcenac <marcenacp@gmail.com>
2019-05-06 14:08:44 +0200

    Template use of Click to handle files

--- base.py
+++ base.py
@@ -2,9 +2,9 @@ import click
 
 
 @click.command()
-@click.option('--argument1', default=1, help='first')
-@click.option('--argument2', default=2, help='second')
-def main(argument1, argument2):
+@click.option('--argument', default=1, help='argument')
+@click.option('--file', default='./file', type=click.File('rb'), help='file')
+def main(argument, file):
     pass
 
 
